a39ebe764e945c17f103fdcbb63e2fb9505cb0fa
Colloquial <47216715+JCGithu@users.noreply.github.com>
2021-10-04 10:31:05 +0100

    patch in lurk message

--- nightbot.js
+++ nightbot.js
@@ -1,8 +1,8 @@
 function runNightBot(){
     const lurkMessage = [
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultrices ac dolor ut accumsan. Maecenas tristique turpis in odio laoreet congue. Sed cursus varius ex sed feugiat. Cras imperdiet nisl.',
-        'more lurk more lurkmore lurkmore lurkmore lurk more lurk more lurk more lurk more lurk v more lurk more lurk more lurk more lurk more lurk v more lurk vmore lurk',
-        'Lurk 3'];
+        'has happened upon a fork in the road. They look left, then glance right. With a deep breath, they begin walking down the path to the left, heading into the Lurking Woods.',
+        'is continuing to lurk yes yes yes',
+        'has found a test lurk message, lucky you!'];
     return lurkMessage[Math.floor(Math.random() * lurkMessage.length)];
 }
 
