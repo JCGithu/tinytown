66f3945f6136e9082eaa6b194862aa988b66af8c
Colloquial <47216715+JCGithu@users.noreply.github.com>
2021-10-04 09:36:06 +0100

    added test strings

--- nightbot.js
+++ nightbot.js
@@ -1,5 +1,9 @@
 function runNightBot(){
-    return 'yes yes this works';
+    const lurkMessage = [
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultrices ac dolor ut accumsan. Maecenas tristique turpis in odio laoreet congue. Sed cursus varius ex sed feugiat. Cras imperdiet nisl.',
+        'more lurk more lurkmore lurkmore lurkmore lurk more lurk more lurk more lurk more lurk v more lurk more lurk more lurk more lurk more lurk v more lurk vmore lurk',
+        'Lurk 3'];
+    return lurkMessage[Math.floor(Math.random() * lurkMessage.length)];
 }
 
 runNightBot();
