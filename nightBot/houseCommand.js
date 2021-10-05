12c1763fd4205a1a800b2ee69f7593f42ff8a4ab
Colloquial <47216715+JCGithu@users.noreply.github.com>
2021-10-05 13:30:24 +0100

    add more info

--- nightBot/houseCommand.js
+++ nightBot/houseCommand.js
@@ -1,17 +1,17 @@
 let houseData = {
-    'test': {
-        "name": "yes",
+    'colloquialowl': {
+        "name": "Owl House",
         "district": "Main"
     }
 }
 
 function houseCommand(user){
     if (houseData[user]){
-        let district = houseData[user];
+        let district = houseData[user].district;
         let name = `called ${houseData[user].name}` || '';
-        return `@${user} has a house called ${name} in the ${district} district`
+        return `@${user} has a house called ${name} in the ${district} district!`
     }
     return 'Sorry, no data for this user yet!'
 }
 
-houseCommand(toUser);
+houseCommand(touser.toLowerCase());
