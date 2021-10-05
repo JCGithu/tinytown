672d5e9d6912ddae216a6b2335161d95f9859a21
Colloquial <47216715+JCGithu@users.noreply.github.com>
2021-10-05 14:09:55 +0100

    added grammar

--- nightBot/houseCommand.js
+++ nightBot/houseCommand.js
@@ -5,14 +5,14 @@ function houseCommand(){
             "name": "Owl House",
             "district": "Main"
         }
-    }    
+    };
 
     if (houseData[user]){
         let district = houseData[user].district;
         let name = `called ${houseData[user].name}` || '';
-        return `@${user} has a house called ${name} in the ${district} district!`
+        return `@${user} has a house called ${name} in the ${district} district!`;
     }
-    return 'Sorry, no data for this user yet!'
-}
+    return 'Sorry, no data for this user yet!';
+};
 
 houseCommand();
