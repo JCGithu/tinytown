2c93db83ca6efa627ba1b6c7ce34aad74e4f66f2
Colloquial <47216715+JCGithu@users.noreply.github.com>
2021-10-05 14:20:55 +0100

    added num + winsord

--- nightBot/houseCommand.js
+++ nightBot/houseCommand.js
@@ -2,15 +2,21 @@ function houseCommand(){
     let user = touser.toLowerCase();
     if (houseData[user]){
         let district = houseData[user].district;
+        let num = `${houseData[user].num} houses` || 'a house';
         let name = `called ${houseData[user].name}` || '';
-        return `@${user} has a house called ${name} in the ${district} district!`;
+        return `@${user} has ${num} ${name} in the ${district}!`;
     }
     return 'Sorry, no data for this user yet!';
 };
 let houseData = {
     "colloquialowl": {
         "name": "Owl House",
-        "district": "Main"
+        "district": "Main District"
+    },
+    "winsord": {
+        "name": "Tall and Tiny",
+        "district": "Main and Raid districts",
+        "num": 2
     }
 };
 houseCommand();
