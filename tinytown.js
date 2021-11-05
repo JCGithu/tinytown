bef2aa909f53e86c1474e09b4d292512d7e31153
JCGithu <jack.c.gracie@gmail.com>
2021-11-05 15:48:46 +0000

    code restructure

--- tinytown.js
+++ tinytown.js
@@ -26,11 +26,11 @@ let fireArray = ['Fire01.svg', 'Fire04.svg', 'Fire05.svg'];
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
-  }
+}
 
 function sizeReset(){
     townContainer.style.height = `${innerHeight - menuSize.bottom}px`;
-    let ratio = 1.257;
+    const ratio = 1.257;
     town.style.height = `${innerWidth / ratio}px`;
     town.style.width = `${innerWidth}px`;
     if ((townSize.top + (innerWidth * (1/ratio))) >= innerHeight){
@@ -77,134 +77,119 @@ class House {
         this.date = houseData.date || undefined;
         this.img = houseData.img;
         this.notes = houseData.notes;
-        this.col1 = houseData.col1 || 'b38046';
-        this.col2 = houseData.col2 || 'faf28a';
+        this.col1 = tinycolor(houseData.col1).toHexString() || 'b38046';
+        this.col2 = tinycolor(houseData.col2).toHexString() || 'faf28a';
+        this.col3 = tinycolor(houseData.col1).analogous() || tinycolor('b38046').analogous();
         this.scale = houseData.scale || 1;
         this.data = houseData.data;
         this.crop = houseData.crop;
         this.zoom = false;
-    };
-
-    init(){
-        let colourScale = tinycolor(this.col1).analogous();
-        this.col1 = tinycolor(this.col1).toHexString();
-        this.col2 = tinycolor(this.col2).toHexString();
-
-        let imgDefaultStyle = {
+        this.houseBox = undefined;
+        this.house = undefined;
+        this.fire = undefined;
+        this.defaultStyle = {
             bottom: `${this.y * gridH}px`,
             width: `${this.scale * gridW}px`,
             height: `${this.scale * gridW}px`,
             zIndex: this.z
-        }
+        };
+    };
 
-        let factDefaultStyle = {
-            opacity: 1,
-            backgroundColor: this.col2
-        }
-
-        if (this.left !== undefined) imgDefaultStyle.left = `${this.left * gridW}px`;
-        if (this.right !== undefined) imgDefaultStyle.right = `${this.right * gridW}px`;
-        
-        let img;
+    init(){
+        if (this.left !== undefined) this.defaultStyle.left = `${this.left * gridW}px`;
+        if (this.right !== undefined) this.defaultStyle.right = `${this.right * gridW}px`;
+    
         if (onMobile){
-            img = document.createElement('object');
-            img.data = `./houseData/images/${this.img}`;
-            img.type = 'image/svg+xml';
+            this.house = document.createElement('embed');
+            this.house.src = `./houseData/images/${this.img}`;
         } else {
-            img = document.createElement('img');
-            img.src = `./houseData/images/${this.img}`;
-        } 
-        img.classList.add('townItem');
-        Object.assign(img.style, imgDefaultStyle);
+            this.house = document.createElement('img');
+            this.house.src = `./houseData/images/${this.img}`;
+        }
+        this.house.classList.add('townItem');
+        Object.assign(this.house.style, this.defaultStyle);
 
         if (this.crop){
             readTextFile(`./houseData/images/${this.img}`, (text) => {
                 let findings = text.match(/(?<=viewBox\=\"0.0.)([\d\.\s]+)/g)[0].split(' ');
-                imgDefaultStyle.width = `${(this.scale * (findings[0]/findings[1])) * gridW}px`
-                Object.assign(img.style, imgDefaultStyle);
+                this.house.style.width = `${(this.scale * (findings[0]/findings[1])) * gridW}px`;
+                this.defaultStyle.width = this.house.style.width;
             })
         }
+        town.appendChild(this.house);
+    }
 
-        let fireON = false;
-        let fireSHOW = false;
-
-        let fire = document.createElement('img');
-        fire.style.width = `${this.scale * gridW * 0.35}px`;
-        fire.style.zIndex = 21;
-        fire.classList.add('townItem');
-        fire.style.pointerEvents = 'none';
-        fire.style.transform = `scale(0)`;
-        let fireReboot = () => {
-            if (this.left !== undefined) fire.style.left = `${this.left * gridW + getRandomInt((this.scale * gridW)*0.5)}px`;
-            if (this.right !== undefined) fire.style.right = `${this.right * gridW + getRandomInt((this.scale * gridW)*0.5)}px`;
-            fire.style.bottom = `${(this.y * gridH) + (this.scale * gridW * 0.4) + getRandomInt(this.scale * gridW * 0.1)}px`;
-            fire.src = `./houseData/images/${fireArray[getRandomInt(fireArray.length)]}`;
-        }
-
-        img.addEventListener('mouseenter', event => {
-            if(!this.data || zoomed) return;
-            factBox.style.opacity = 1;
-            factBox.style.top= `${event.clientY}px`;
-            factBox.style.left= `${event.clientX}px`;
-            if (!this.zoom) img.style.transform = "scale(1.1)";
-            img.style.zIndex = 20;
-            factTitle.innerHTML = this.title;
-            factOwner.innerHTML = this.owner;
-            factNotes.innerHTML = this.notes;
-            factBanner.style.backgroundColor = this.col1;
-            factBanner.style.color = this.col2;
-            factOwner.style.color = this.col2;
-            factBox.style.backgroundColor = this.col2
-            factOwner.style.backgroundColor = colourScale[2].toHexString();
-            factNotes.style.color = this.col1;
-            onScreenCheck(factBox);
-            document.body.onkeyup = (e) => {
-                if(e.keyCode == 32){
-                    fireSHOW = !fireSHOW;
-                    if (!fireON){
-                        town.appendChild(fire);
-                        fireON = true; 
-                    }
-                    if (fireSHOW) {
-                        fire.style.transform = `scale(1)`;
-                    } else {
-                        fire.style.transform = `scale(0)`;
-                    }
-                    fireReboot();
+    hover(event){
+        if(!this.data || zoomed) return;
+        factBox.style.opacity = 1;
+        factBox.style.top= `${event.clientY}px`;
+        factBox.style.left= `${event.clientX}px`;
+        if (!this.zoom) this.house.style.transform = "scale(1.1)";
+        this.house.style.zIndex = 20;
+        factTitle.innerHTML = this.title;
+        factOwner.innerHTML = this.owner;
+        factNotes.innerHTML = this.notes;
+        factBanner.style.backgroundColor = this.col1;
+        factBanner.style.color = this.col2;
+        factOwner.style.color = this.col2;
+        factBox.style.backgroundColor = this.col2
+        factOwner.style.backgroundColor = this.col3[2].toHexString();
+        factNotes.style.color = this.col1;
+        onScreenCheck(factBox);
+        if (!this.fire) this.fireGen();
+        document.body.onkeyup = (e) => {
+            if(e.keyCode == 32){
+                this.fire.style.visibility = 'visible';
+                if (this.fire.style.transform === 'scale(0)') {
+                    console.log('change');
+                    this.fire.style.transform = `scale(1)`;
+                    return;
                 }
+                this.fire.style.transform = `scale(0)`;
             }
-        });
+        }
+    }
 
-        img.addEventListener('mouseleave', () => {
-            if(!this.data || zoomed) return;
-            factBox.style.opacity = 0;
-            img.style.transform = "scale(1)";
-            if(!this.zoom) img.style.zIndex = this.z;
-        })
+    fireGen(){
+        this.fire = document.createElement('img');
+        this.fire.classList.add('fire');
+        this.fire.style.transform = `scale(0)`;
+        this.fire.style.width = `${this.scale * gridW * 0.35}px`;
+        if (this.left !== undefined) this.fire.style.left = `${this.left * gridW + getRandomInt((this.scale * gridW)*0.5)}px`;
+        if (this.right !== undefined) this.fire.style.right = `${this.right * gridW + getRandomInt((this.scale * gridW)*0.5)}px`;
+        this.fire.style.bottom = `${(this.y * gridH) + (this.scale * gridW * 0.4) + getRandomInt(this.scale * gridW * 0.1)}px`;
+        this.fire.src = `./houseData/images/${fireArray[getRandomInt(fireArray.length)]}`;
+        town.appendChild(this.fire);
+    }
 
-        img.addEventListener('click', () => {
-            if(!this.data) return;
-            if(!this.zoom){
-                if (zoomed) return;
-                img.classList.add('zoomedImage');
-                factBox.classList.add('factZoom');
-                img.style.width = `${townSize.height}px`;
-                img.style.height = `${townSize.height}px`;
-                img.style.left = `${(townSize.width - townSize.height)/2}px`;
-                this.zoom = true, zoomed = true;
-                blurry.style.backgroundColor = tinycolor(this.col1).darken().toHexString();
-                blurry.classList.add('blurryOn');
-            } else {
-                Object.assign(img.style, imgDefaultStyle);
-                this.zoom = false, zoomed = false;
-                img.classList.remove('zoomedImage');
-                factBox.classList.remove('factZoom');
-                blurry.classList.remove('blurryOn');
-            }
-        })
+    unhover(){
+        if(!this.data || zoomed) return;
+        factBox.style.opacity = 0;
+        this.house.style.transform = "scale(1)";
+        if(!this.zoom) this.house.style.zIndex = this.z;
+    }
 
-        town.appendChild(img);
-    } 
+    click(){
+        console.log('ZOOM!');
+        if(!this.data) return;
+        if(!this.zoom){
+            if (zoomed) return;
+            this.house.classList.add('zoomedImage');
+            factBox.classList.add('factZoom');
+            this.house.style.width = `${townSize.height}px`;
+            this.house.style.height = `${townSize.height}px`;
+            this.house.style.left = `${(townSize.width - townSize.height)/2}px`;
+            this.zoom = true, zoomed = true;
+            blurry.style.backgroundColor = tinycolor(this.col1).darken().toHexString();
+            blurry.classList.add('blurryOn');
+            return;
+        }
+        Object.assign(this.house.style, this.defaultStyle);
+        this.zoom = false, zoomed = false;
+        this.house.classList.remove('zoomedImage');
+        factBox.classList.remove('factZoom');
+        blurry.classList.remove('blurryOn');
+    }
 }
 
 let districts = ['mainDistrict', 'raidDistrict', 'lurkDistrict', 'marblesDistrict', 'originsDistrict'];
@@ -218,6 +203,9 @@ function loadDistrict(district){
         for (let house in data){
             let input = new House(data[house]);
             input.init();
+            input.house.addEventListener('mouseenter', (event) => input.hover(event));
+            input.house.addEventListener('mouseleave', (event) => input.unhover(event));
+            input.house.addEventListener('click', (event) => input.click(event));
         }
         currentDistrict = district;
     })
@@ -234,7 +222,14 @@ districts.forEach(district => {
 });
 
 window.addEventListener('resize', () => {
-    zoomed = false;
+    if (zoomed){
+        //Object.assign(this.house.style, this.defaultStyle);
+        this.zoom = false, zoomed = false;
+        //this.house.classList.remove('zoomedImage');
+        factBox.classList.remove('factZoom');
+        factBox.style.opacity = 0;
+        blurry.classList.remove('blurryOn');
+    }
     town.querySelectorAll('*').forEach(n => n.remove());
     loadDistrict(currentDistrict)
 });
